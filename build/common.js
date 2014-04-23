fa07995959d2b46056bec86504630a88daf19695
xiaoqing.dongxq <xiaoqing.dongxq@alibaba-inc.com>
2014-04-23 14:11:43 +0800

    change subversion

--- build/common.js
+++ build/common.js
@@ -114,7 +114,7 @@ define('bui/util',function(require){
              * 子版本号
              * @type {Number}
              */
-            subVersion : 84,
+            subVersion : 88,
 
             /**
              * 是否为函数
--- build/seed.js
+++ build/seed.js
@@ -1107,7 +1107,7 @@ define('bui/util',function(require){
              * 子版本号
              * @type {Number}
              */
-            subVersion : 84,
+            subVersion : 88,
 
             /**
              * 是否为函数
