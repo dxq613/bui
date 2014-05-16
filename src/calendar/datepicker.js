f87558b82cc55edb7c8c2eada47bf204e185a072
桐人 <zheng.fuz@alibaba-inc.com>
2014-05-17 00:38:44 +0800

    datePicker 增加配置默认日期的属性defaultSelectedDate
    这个属性可以帮助设置日历展开时默认显示的日期，比如在选择出生日期的场景下，把默认的年份设的前面一点，可以让用户更快的选择

--- src/calendar/datepicker.js
+++ src/calendar/datepicker.js
@@ -75,7 +75,8 @@ define('bui/calendar/datepicker',['bui/common','bui/picker','bui/calendar/calend
       var _self = this,
         calendar = this.get('calendar'),
         date = DateUtil.parse(val,_self.get("dateMask"));
-      date = date || new Date(new Date().setSeconds(0));
+      //date = date || new Date(new Date().setSeconds(0));
+      date = date || _self.get('defaultSelectedDate');
       calendar.set('selectedDate',DateUtil.getDate(date));
       if(_self.get('showTime')){
           var lockTime = this.get("lockTime"),
@@ -219,6 +220,13 @@ define('bui/calendar/datepicker',['bui/common','bui/picker','bui/calendar/calend
        */
       calendar:{
 
+      },
+      /**
+       * 默认选中的日期
+       * @type {Date}
+       */
+      defaultSelectedDate: {
+      	value: BUI.Date.today()
       }
     }
   },{
